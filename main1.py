c8dddeecf6cce9c0f94714f1f976ea47b838fcdf
Desi Bakar <desybakar12@gmail.com>
2024-10-05 15:27:37 +0700

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,18 @@
+print("hello word")
+# ini komentar sebagai reminder saja. misalkan jika ada yang habis telah di update dan # itu hanya komentar untuk 1 baris
+'''
+ini komentar
+ini komentar untuk 2 baris menggunakan kutip 1 sebanyak 3
+'''
+data_1 = "hello word Desi Ratnasari"
+data_2 = "belajar lagi yukkk"
+# kutip "" ini tipe data string
+
+print(data_2)
+
+
+
+
+
+
+
