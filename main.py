b7b4af39f6cafd75edcc0b58baf4a54d005f230e
Desi-Bakar <desybakar12@gmail.com>
2024-10-05 15:04:32 +0700

    add repository

---
+++ main.py
@@ -0,0 +1,57 @@
+# mendefinisikan variabel
+# angka1 = 10
+# angka2 = 5
+# angka3 = 1
+
+# operasi penjulahan
+# hasil_penjumlahan = angka1 + angka2 + angka3
+# print("Hasil penjumlahan:", hasil_penjumlahan)
+
+# # operasi pengurangan
+# hasil_pengurangan = angka1 - angka2 - angka3
+# print("Hasil pengurangan:", hasil_pengurangan)
+
+# # operasi perkalian
+# hasil_perkalian = angka1 * angka2 * angka3
+# print("Hasil perkalian:", hasil_perkalian)
+
+# # operasi pembagian
+# hasil_pembagian = angka1 / angka2 / angka3
+# print("Hasil pembagian:", hasil_pembagian)
+
+# # operasi pemangkatan
+# hasil_pemangkatan = angka1 ** angka2 ** angka3
+# print("Hasil pemangkatan:", hasil_pemangkatan)
+
+
+# # contoh operasi perbandingan
+# angka1 = 10
+# angka2 = 5
+
+# #operator sama dengan (==)
+# print(angka1 == angka2) #output : False
+
+# #operator tidak sama dengan (!=)
+# print(angka1 != angka2) #output : True
+
+# #operator lebih besar dari (>)
+# print(angka1 > angka2) #output : True
+
+# #operator lebih kecil dari (<)
+# print(angka1 < angka2) #output : False
+
+# #operator lebih besar sama dengan (>=)
+# print(angka1 >= angka2) #output : False
+
+# #operator lebih kecil sama dengan (<=)
+# print(angka1 <= angka2) #output : False
+
+
+#chalange
+nilai1 = 15
+nilai2 = 10
+nilai3 = 20
+
+print(nilai2 == nilai3)
+print(nilai1 >= nilai3)
+print(nilai1 < nilai3)
